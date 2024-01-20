c607230f513a5672caa1f3b767a88c55180263a9
Hanif Hakim <hanif.hakim@spesolution.com>
2024-01-20 20:56:02 +0700

    Rearrange project structure

--- src/main/java/com/hakimfauzi23/boilerplatespringsecurity/modules/auth/data/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.hakimfauzi23.boilerplatespringsecurity.modules.auth.data;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
---
+++ src/main/java/com/hakimfauzi23/boilerplatespringsecurity/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package com.hakimfauzi23.boilerplatespringsecurity.repository;
+
+import com.hakimfauzi23.boilerplatespringsecurity.data.RefreshToken;
+import com.hakimfauzi23.boilerplatespringsecurity.data.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
